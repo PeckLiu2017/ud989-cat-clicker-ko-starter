789c802dc80a2bf7d62a5ebfb493d1dfb141e6a6
peck liu <xiaoyang20170204@gmail.com>
2017-11-15 20:35:31 +0800

    implement viewmodel

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var ViewModel = function () {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imageAttribution = ko.observable('https://www.flickr.com/photos/big');
+  this.incrementCounter = function () {
+    this.clickCount(this.clickCount() + 1);
+    var count = 0;
+    count++;
+  }
+}
+
+ko.applyBindings(new ViewModel());
