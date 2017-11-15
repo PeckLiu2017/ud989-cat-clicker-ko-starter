5a1f968426f4b43f4f3d583b806c41158537a08c
peck liu <xiaoyang20170204@gmail.com>
2017-11-15 23:28:41 +0800

    use with to reduce html code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var ViewModel = function () {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imageAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
-  this.nicknames = ko.observableArray(['Tabtab','T-Bone','Mr.T','Tabitha Tab Tabby Catty Cat']);
+var Cat = function (data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imageAttribution = ko.observable(data.imageAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.title = ko.computed(function () {
     var title;
@@ -23,11 +23,20 @@ var ViewModel = function () {
     }
     return title;
   },this);
+}
 
+var ViewModel = function () {
+  this.currentCat = ko.observable( new Cat({
+    clickCount : 0,
+    name : 'Tabby',
+    imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+    imageAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nicknames : ['Tabtab','T-Bone','Mr.T','Tabitha Tab Tabby Catty Cat']
+  }) );
   this.incrementCounter = function () {
+    // 当用了 with 之后，以 view 为准，我们已经身处 Cat 的上下文之中，这里的 this 指的是 Cat
     this.clickCount(this.clickCount() + 1);
-    var count = 0;
-    count++;
+    // 或者将 ViewModel 储存在 self 中，self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   }
 }
 
