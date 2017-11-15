48320680335fc1ada3f2be2917a3cf7201bac941
peck liu <xiaoyang20170204@gmail.com>
2017-11-15 21:16:41 +0800

    add this.title in ViewModel

--- js/app.js
+++ js/app.js
@@ -2,7 +2,28 @@ var ViewModel = function () {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imageAttribution = ko.observable('https://www.flickr.com/photos/big');
+  this.imageAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+  this.nicknames = ko.observableArray(['Tabtab','T-Bone','Mr.T','Tabitha Tab Tabby Catty Cat']);
+
+  this.title = ko.computed(function () {
+    var title;
+    var clicks = this.clickCount();
+    if (clicks < 10) {
+      title = 'Newborn';
+    } else if (clicks < 50) {
+      title = 'Infant';
+    } else if (clicks < 100) {
+      title = 'Child';
+    } else if (clicks < 200) {
+      title = 'Teen';
+    } else if (clicks < 500) {
+      title = 'Adult';
+    } else {
+      title = 'Ninja';
+    }
+    return title;
+  },this);
+
   this.incrementCounter = function () {
     this.clickCount(this.clickCount() + 1);
     var count = 0;
